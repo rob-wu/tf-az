16c821de826dc29cce5f1a5061816c813ee23df4
Rob Wu <rw@brigadecapital.com>
2025-03-23 14:57:04 -0400

    linting

--- sql-webapp01.tf
+++ sql-webapp01.tf
@@ -1,8 +1,8 @@
 resource "azurerm_mssql_server" "sql-webapp01" {
-  name                         = "sql-webapp01"
-  resource_group_name          = azurerm_resource_group.rg-webapp01.name
-  location                     = azurerm_resource_group.rg-webapp01.location
-  version                      = "12.0"
+  name                = "sql-webapp01"
+  resource_group_name = azurerm_resource_group.rg-webapp01.name
+  location            = azurerm_resource_group.rg-webapp01.location
+  version             = "12.0"
   # secrets pulled from Azure KV
   administrator_login          = data.azurerm_key_vault_secret.sql-user.value
   administrator_login_password = data.azurerm_key_vault_secret.sql-passwd.value
